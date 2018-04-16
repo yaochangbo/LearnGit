0d41ba186a302ce4e596800fe8620d3e9c871cff
daqi.zuo <daqi.zuo@luckincoffee.com>
2018-04-16 15:45:53 +0800

    this is first commit

---
+++ src/main/java/LearnGit.java
@@ -0,0 +1,10 @@
+/**
+ * Created by changbo.yao.
+ * Date: 2018/4/16
+ * Time: ${Time}
+ */
+public class LearnGit {
+    public static void main(String[] args) {
+        System.out.println("this is root");
+    }
+}
