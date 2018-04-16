32c28c46c175fe0c47e249aa84918e8e14b4d24a
daqi.zuo <daqi.zuo@luckincoffee.com>
2018-04-16 16:46:47 +0800

    this is 4 commit

--- src/main/java/LearnGit.java
+++ src/main/java/LearnGit.java
@@ -7,5 +7,6 @@ public class LearnGit {
     public static void main(String[] args) {
         System.out.println("this is root");
         System.out.println("this is second");
+        System.out.println("41");
     }
 }
