9747405b6337b383a8df5933d4f2494aba2a17b8
daqi.zuo <daqi.zuo@luckincoffee.com>
2018-04-16 16:52:29 +0800

    Merge branch 'master' of https://github.com/yaochangbo/LearnGit
    # Conflicts:
    #	src/main/java/LearnGit.java

--- src/main/java/LearnGit.java
+++ src/main/java/LearnGit.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by changbo.yao.
  * Date: 2018/4/16
@@ -11,5 +13,6 @@ public class LearnGit {
         System.out.println("41");
         System.out.println("666661");
         System.out.println("555551");
+        System.out.println("--------------this is branch 01---------");
     }
 }
